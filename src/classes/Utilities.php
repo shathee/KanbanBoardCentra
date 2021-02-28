0ee3781a25d2466de9a06cb25e6cd257ea453df6
Baki <sat.sust@gmail.com>
2021-02-28 22:54:04 +0100

    function added for loading .env file contents

--- src/classes/Utilities.php
+++ src/classes/Utilities.php
@@ -6,6 +6,46 @@ class Utilities
 	private function __construct() {
 	}
 
+	/**
+	 * Loads the parameter values from .env file to $_ENV global  
+	 * @param type $file_path 
+	 * @return 
+	*/
+	
+	public static function load_env_file($file_path) {
+		
+		if(!file_exists($file_path)) {
+            throw new \InvalidArgumentException(sprintf('%s does not exist', $file_path));
+        }else{
+        	if (!is_readable($file_path)) {
+            	throw new \RuntimeException(sprintf('%s file is not readable', $file_path));
+        	}
+        	$lines = file($file_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+	        foreach ($lines as $line) {
+
+	            if (strpos(trim($line), '#') === 0) {
+	                continue;
+	            }
+	            list($env_var_name, $env_var_value) = explode('=', $line, 2);
+	            $env_var_name = trim($env_var_name);
+	            $env_var_value = trim($env_var_value);
+
+	            if (!array_key_exists($env_var_name, $_SERVER) && !array_key_exists($env_var_name, $_ENV)) {
+	                putenv(sprintf('%s=%s', $env_var_name, $env_var_value));
+	                $_ENV[$env_var_name] = $env_var_value;
+	                $_SERVER[$env_var_name] = $env_var_value;
+	            }
+	        }
+        }
+	}
+
+	/**
+	 * gets the value of the environment parameter 
+	 * @param type $name 
+	 * @param type|null $default 
+	 * @return 
+	 */
+	
 	public static function env($name, $default = NULL) {
 		$value = getenv($name);
 		if($default !== NULL) {
--- src/public/index.php
+++ src/public/index.php
@@ -7,6 +7,10 @@ require '../classes/KanbanBoard/Github.php';
 require '../classes/Utilities.php';
 require '../classes/KanbanBoard/Authentication.php';
 
+
+$environment_file_path = dirname(__DIR__, 2).'/.env';
+$env = Utilities::load_env_file($environment_file_path);
+
 $repositories = explode('|', Utilities::env('GH_REPOSITORIES'));
 $authentication = new \KanbanBoard\Login();
 $token = $authentication->login();
