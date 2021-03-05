040663c3b56256e8b47caae70278388610a4b0c9
Baki <sat.sust@gmail.com>
2021-03-05 23:31:30 +0100

    comments added in index

--- src/public/index.php
+++ src/public/index.php
@@ -8,16 +8,19 @@ require '../classes/Utilities.php';
 require '../classes/KanbanBoard/Authentication.php';
 
 
+// setting the .env path 
 $environment_file_path = dirname(__DIR__, 2).'/.env';
-$env = Utilities::load_env_file($environment_file_path);
+
+Utilities::load_env_file($environment_file_path);
+
 
 $repositories = explode('|', Utilities::env('GH_REPOSITORIES'));
 $authentication = new \KanbanBoard\Login();
 $token = $authentication->login();
 $github = new GithubClient($token, Utilities::env('GH_ACCOUNT'));
+
 $board = new \KanbanBoard\Application($github, $repositories, array('waiting-for-feedback'));
 $data = $board->board();
-// print_r($data);
 
 $m = new Mustache_Engine(array(
 	'loader' => new Mustache_Loader_FilesystemLoader('../views'),
