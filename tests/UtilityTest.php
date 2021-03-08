ea828ef9ff879e70b8d459047e54b79c61b5f645
Baki <sat.sust@gmail.com>
2021-03-09 00:39:07 +0100

    utility test updated

--- tests/UtilityTest.php
+++ tests/UtilityTest.php
@@ -8,45 +8,25 @@ final class UtilityTest extends PHPUnit_Framework_TestCase
 	public function setUp()
     {
         $this->env_path = dirname(__DIR__).'/.env';
-
         Utilities::load_env_file(dirname(__DIR__).'/.env');
-        
-    }
-    
-    public function testEnvFileExists(){
-        $this->assertFileExists($this->env_path);
     }
 
-    /**
-     * @runtestEnvFileExists
-     */
-    public function testEnvFileHasGhAccountKey(){
-        $this->assertArrayHasKey('GH_ACCOUNT', $_ENV, "env has no key GH_ACCOUNT");
+    public function testLoadEnvFile(){
+        $this->assertArrayHasKey('GH_ACCOUNT', $_ENV);
     }
-    /**
-     * @runtestEnvFileExists
-     */
-    public function testEnvFileHasGhAccountKeyValue(){
-        $this->assertGreaterThan(0, strlen($_ENV['GH_ACCOUNT']));
+
+    public function testGetEnvWithDefaultValue(){
+        $this->assertNotNull(Utilities::env('GH_ACCOUNT', 'SomeAcc'));
     }
-    
-    /**
-     * @runtestEnvFileExists
-     */
-    public function testEnvFileHasGhClientIdKey(){
-        $this->assertArrayHasKey('GH_CLIENT_ID', $_ENV, "env has no key GH_CLIENT_ID");
-    }
-    /**
-     * @runtestEnvFileExists
-     */
-    public function testEnvFileHasGhClientSecretKey(){
-        $this->assertArrayHasKey('GH_CLIENT_SECRET', $_ENV, "env has no key GH_CLIENT_SECRET");
-    }
-    /**
-     * @runtestEnvFileExists
-     */
-    public function testEnvFileHasGHRepositoryKey(){
-        $this->assertArrayHasKey('GH_REPOSITORIES', $_ENV, "env has no key GH_REPOSITORIES");
-    }
+
+    public function testGetEnvWorksWhenEnvIsLoaded(){
+        $this->assertNotNull(Utilities::env('GH_ACCOUNT'));
+    }    
+
+    public function testHasValue(){
+        $test_arr = array('user' => 'baki', 'git_name' => 'shathee'); 
+        $this->assertTrue(Utilities::hasValue($test_arr, 'user'));
+    }    
+
 
 }
